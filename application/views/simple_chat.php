d4a29c763fef5eb19ecf5b32395da0de49c6c67e
awan21clouds <awan@21clouds.com>
2016-01-13 10:01:59 +0700

    Update simple_chat.php

--- application/views/simple_chat.php
+++ application/views/simple_chat.php
@@ -52,62 +52,19 @@
                             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
                                 <i class="fa fa-bars"></i>
                             </button>
-
                         </div>
-
-                        <!-- Collect the nav links, forms, and other content for toggling -->
-                        <!--                        <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
-                                                    <ul class="nav navbar-nav">
-                                                        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-                                                        <li><a href="#">Link</a></li>
-                                                        <li class="dropdown">
-                                                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
-                                                            <ul class="dropdown-menu" role="menu">
-                                                                <li><a href="#">Action</a></li>
-                                                                <li><a href="#">Another action</a></li>
-                                                                <li><a href="#">Something else here</a></li>
-                                                                <li class="divider"></li>
-                                                                <li><a href="#">Separated link</a></li>
-                                                                <li class="divider"></li>
-                                                                <li><a href="#">One more separated link</a></li>
-                                                            </ul>
-                                                        </li>
-                                                    </ul>
-                                                    <form class="navbar-form navbar-left" role="search">
-                                                        <div class="form-group">
-                                                            <input type="text" class="form-control" id="navbar-search-input" placeholder="Search">
-                                                        </div>
-                                                    </form>
-                                                </div>-->
-
                     </div>
                     <!-- /.container-fluid -->
                 </nav>
             </header>
             <!-- Full Width Column -->
-
             <div class="content-wrapper">
                 <div class="container">
-                    <!-- Content Header (Page header) -->
-<!--                    <section class="content-header">
-                        <h1>
-                            Top Navigation
-                            <small>Example 2.0</small>
-                        </h1>
-                        <ol class="breadcrumb">
-                            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-                            <li><a href="#">Layout</a></li>
-                            <li class="active">Top Navigation</li>
-                        </ol>-->
-                    </section>
-
                     <!-- Main content -->
                     <section class="content">
-
                         <div class="box box-solid direct-chat direct-chat-primary">
                             <div class="box-header with-border">
                                 <i class="fa fa-comments-o"></i>
-
                             </div>
                             <div class="box-body chat" id="chat-box">
                                 <div class="direct-chat-messages">
@@ -141,20 +98,5 @@
             </footer>
         </div>
         <!-- ./wrapper -->
-
-
-
-
-
-<!--        <script type="text/javascript">
-            $(function () {
-                $('#chat-box, .direct-chat-messages').slimScroll({
-                    height: '380px'
-                });
-                var chat = new Chat();
-                chat.setChat();
-                chat.getChat();
-            });
-        </script>-->
     </body>
 </html>
